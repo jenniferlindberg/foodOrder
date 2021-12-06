5d9027fbbafedac1c8273d03b0e2736e53ad1699
jenniferlindberg <jinjin.lindberg@gmail.com>
2021-12-06 19:04:51 +0100

    feat: add `Cart` component, Issue #2

---
+++ src/components/Cart/Cart.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+import classes from "./Cart.module.css";
+
+const Cart = (props) => {
+  const cartItems = (
+    <ul className={classes["cart-items"]}>
+      {[{ id: "c1", name: "sushi", amount: 2, price: 12.99 }].map((item) => (
+        <li>{item.name}</li>
+      ))}
+    </ul>
+  );
+  return (
+    <div className={classes.total}>
+      {cartItems}
+      <div>Total Amount</div>
+      <div>35.62</div>
+      <div className={classes.actions}>
+        <button className={classes["button--alt"]}>Close</button>
+        <button className={classes.button}>Order</button>
+      </div>
+    </div>
+  );
+};
+
+export default Cart;
